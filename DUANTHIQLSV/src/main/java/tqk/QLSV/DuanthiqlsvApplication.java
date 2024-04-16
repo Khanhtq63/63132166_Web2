a6f98f91d960c954589ce70a73198a537450e919
Khanhtq63 <sitanred1234@gmail.com>
2024-04-16 15:03:28 +0700

    update

---
+++ DUANTHIQLSV/src/main/java/tqk/QLSV/DuanthiqlsvApplication.java
@@ -0,0 +1,13 @@
+package tqk.QLSV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuanthiqlsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuanthiqlsvApplication.class, args);
+	}
+
+}
---
+++ DUANTHIQLSV/src/test/java/tqk/QLSV/DuanthiqlsvApplicationTests.java
@@ -0,0 +1,13 @@
+package tqk.QLSV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuanthiqlsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
