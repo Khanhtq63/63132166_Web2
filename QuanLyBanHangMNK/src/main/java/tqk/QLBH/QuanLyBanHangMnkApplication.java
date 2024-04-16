82a7d32b8a301745c480753e8e118afe738d3bb5
Khanhtq63 <sitanred1234@gmail.com>
2024-04-16 14:03:04 +0700

    update

---
+++ QuanLyBanHangMNK/src/main/java/tqk/QLBH/QuanLyBanHangMnkApplication.java
@@ -0,0 +1,13 @@
+package tqk.QLBH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyBanHangMnkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanLyBanHangMnkApplication.class, args);
+	}
+
+}
---
+++ QuanLyBanHangMNK/src/test/java/tqk/QLBH/QuanLyBanHangMnkApplicationTests.java
@@ -0,0 +1,13 @@
+package tqk.QLBH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLyBanHangMnkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
