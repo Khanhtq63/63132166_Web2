32ff34e7f84c448f2dadd27c990c921663796299
Khanhtq63 <sitanred1234@gmail.com>
2024-04-23 15:50:04 +0700

    update

---
+++ DUANTHIQLSV/src/main/java/tqk/QLSV/Controllers/AdminController.java
@@ -0,0 +1,34 @@
+package tqk.QLSV.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class AdminController {
+	@GetMapping("/")
+	public String Dashboard() {
+		return "Dashboard.html";
+	}
+	@GetMapping("/Student")
+	public String Student() {
+		return "Student.html";
+	}
+	@GetMapping("/Course")
+	public String Course() {
+		return "Course.html";
+	}
+	@GetMapping("/Class")
+	public String Class() {
+		return "Class.html";
+	}
+	@GetMapping("/Department")
+	public String Department() {
+		return "Department.html";
+	}
+	@GetMapping("/Score")
+	public String Score() {
+		return "Score.html";
+	}
+}
+
