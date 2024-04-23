023d6e00b4321006d8cbb9d89c84b03113cbef78
Khanhtq63 <sitanred1234@gmail.com>
2024-04-23 16:11:21 +0700

    update

--- DUANTHIQLSV/src/main/java/tqk/QLSV/Controllers/AdminController.java
+++ DUANTHIQLSV/src/main/java/tqk/QLSV/Controllers/AdminController.java
@@ -10,25 +10,25 @@ public class AdminController {
 	public String Dashboard() {
 		return "index.html";
 	}
-	@GetMapping("/Student")
-	public String Student() {
-		return "Student.html";
+	@GetMapping("/sinhvien")
+	public String SinhVien() {
+		return "SinhVien.html";
 	}
-	@GetMapping("/Course")
-	public String Course() {
-		return "Course.html";
+	@GetMapping("/khoahoc")
+	public String KhoaHoc() {
+		return "KhoaHoc.html";
 	}
-	@GetMapping("/Class")
-	public String Class() {
-		return "Class.html";
+	@GetMapping("/lop")
+	public String Lop() {
+		return "Lop.html";
 	}
-	@GetMapping("/Department")
-	public String Department() {
-		return "Department.html";
+	@GetMapping("/monhoc")
+	public String MonHoc() {
+		return "MonHoc.html";
 	}
-	@GetMapping("/Score")
-	public String Score() {
-		return "Score.html";
+	@GetMapping("/diem")
+	public String Diem() {
+		return "Diem.html";
 	}
 }
 
