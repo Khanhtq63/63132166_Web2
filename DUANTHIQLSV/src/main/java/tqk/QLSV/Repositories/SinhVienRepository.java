7934aeaf85cbe4f3f31739299ca98f09c1d1608d
Khanhtq63 <sitanred1234@gmail.com>
2024-04-23 13:58:58 +0700

    update

--- DUANTHIQLSV/src/main/java/tqk/QLSV/Repositories/SinhVienRepository.java
+++ DUANTHIQLSV/src/main/java/tqk/QLSV/Repositories/SinhVienRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import tqk.QLSV.Models.SinhVienModel;
 
 public interface SinhVienRepository extends JpaRepository<SinhVienModel, String> {
+	
 
 }
